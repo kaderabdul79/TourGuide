2ebecb74f7bf600b22bcb2ddb987db9013c50a28
kaderabdul <kaderabdul7980@gmail.com>
2022-01-01 19:23:42 +0600

    include material UI and add container,grid and paper

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import TourCard from './TourCard/TourCard';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 const Home = () => {
     return (
         <div>
             this is home
+            <Container>
+                <Grid container spacing={2}>
+                    <TourCard></TourCard>
+                </Grid>
+            </Container>
+            
         </div>
     );
 };
---
+++ src/components/Home/TourCard/TourCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const TourCard = () => {
+    return (
+        <Grid item xs={4}>
+            <Paper elevation={3} variant="outlined" >
+                TourCard
+            </Paper>
+        </Grid>
+        
+    );
+};
+
+export default TourCard;
